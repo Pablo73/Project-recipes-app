e65ada1dc1c5f5534647df5364068b0cb8a4d0f9
HigoJhon <higojhon98@gmail.com>
2023-01-25 20:19:38 -0300

    criando test para Header

---
+++ src/tests/Header.test.js
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Meals from '../pages/Meals';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando componente Hearder', () => {
+  test('testando tela', () => {
+    const { history } = renderWithRouter(<Meals />);
+
+    const pageTitle = screen.getByTestId('page-title');
+    expect(pageTitle).toBeInTheDocument();
+
+    const pageProfile = screen.getByTestId('profile-top-btn');
+    expect(pageProfile).toBeInTheDocument();
+
+    const btnSearch = screen.getByTestId('search-top-btn');
+    expect(btnSearch).toBeInTheDocument();
+
+    userEvent.click(pageProfile);
+
+    expect(history.location.pathname).toBe('/profile');
+  });
+});
