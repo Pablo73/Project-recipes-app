899e3ab4bb46c7a95468f516ebd86e9b4b72f30e
liviaffm <liviaafernandaafariaas@gmail.com>
2023-01-26 18:01:34 -0300

    cria a tela de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+
+  const user = JSON.parse(localStorage.getItem('user')) || '';
+
+  function toggleDoneRecipes(path) {
+    history.push(path);
+  }
+  function toggleFavoriteRecipes(path) {
+    history.push(path);
+  }
+  function toggleLogout(path) {
+    history.push(path);
+    localStorage.clear('userProfile');
+  }
   return (
     <div>
       <header>
@@ -11,6 +26,30 @@ function Profile() {
           withSearchIcon={ false }
         />
       </header>
+      <div>
+        <p data-testid="profile-email">{user.email}</p>
+        <button
+          data-testid="profile-done-btn"
+          onClick={ () => toggleDoneRecipes('/done-recipes') }
+        >
+          Done Recipes
+
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          onClick={ () => toggleFavoriteRecipes('/favorite-recipes') }
+        >
+          Favorite Recipes
+
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          onClick={ () => toggleLogout('/') }
+        >
+          Logout
+
+        </button>
+      </div>
       <Footer />
     </div>
   );
