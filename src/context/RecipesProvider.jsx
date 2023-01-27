9361721c8fbeae9ad8c547d371f9830d3b3d66ac
luc0liv <luoliveiradacunha@gmail.com>
2023-01-26 23:36:36 -0300

    REQ 12: Realizando pesquisa de acordo com a rota

--- src/context/RecipesProvider.jsx
+++ src/context/RecipesProvider.jsx
@@ -1,32 +1,42 @@
 import PropTypes from 'prop-types';
 import { useMemo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import RecipesContext from './RecipesContext';
 
 function RecipesProvider({ children }) {
   const [searchFood, setSearchFood] = useState([]);
+  const location = useLocation();
 
-  const handleFetch = async (searchType, searchTerm) => {
-    console.log(searchType);
-    const object = {
-      ingredient: `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`,
-      name: `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`,
-      firstLetter: `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchTerm}`,
-    };
-
-    const handleSearch = object[searchType];
-
-    const responseFood = await fetch(handleSearch);
-
-    const results = await responseFood.json();
-
-    setSearchFood(results);
-    console.log(results);
+  const defineFetchApi = (searchType, searchTerm) => {
+    if (location.pathname.includes('meals')) {
+      const mealsApi = {
+        ingredient: `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`,
+        name: `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`,
+        firstLetter: `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchTerm}`,
+      };
+      return mealsApi[searchType];
+    }
+    if (location.pathname.includes('drinks')) {
+      const drinksApi = {
+        ingredient: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchTerm}`,
+        name: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`,
+        firstLetter: `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchTerm}`,
+      };
+      return drinksApi[searchType];
+    }
   };
 
-  // const contexValue = {
-  //   handleFetch,
-  //   searchFood,
-  // };
+  const handleFetch = async (searchType, searchTerm) => {
+    try {
+      const apiUrl = defineFetchApi(searchType, searchTerm);
+      const responseFood = await fetch(apiUrl);
+      const results = await responseFood.json();
+      setSearchFood(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const value = useMemo(() => ({
     handleFetch,
     searchFood,
