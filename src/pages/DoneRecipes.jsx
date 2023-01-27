81f372325ea41984cf254b747b3265faa552b1c8
HigoJhon <higojhon98@gmail.com>
2023-01-27 17:41:21 -0300

    criando elementos para o componente DonaRecipes

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -10,6 +10,31 @@ function DoneRecipes() {
           withSearchIcon={ false }
         />
       </header>
+      <filter>
+        <button data-testid="filter-by-all-btn">All</button>
+        <button data-testid="filter-by-meal-btn">Meals</button>
+        <button data-testid="filter-by-drink-btn">Drinks</button>
+        <img
+          // data-testid="${index}-horizontal-image"
+          src="image"
+          alt="name"
+        />
+        <div>
+          {/* data-testid="${index}-horizontal-image" */}
+          <h1>
+            {/* data-testid="${index}-horizontal-name" */}
+            O texto do nome da receita
+          </h1>
+          <p>
+            {/* data-testid="${index}-horizontal-done-date" */}
+            O texto da data que a receita
+          </p>
+          <input
+            type="button"
+            // data-testid="${index}-horizontal-share-btn"
+          />
+        </div>
+      </filter>
     </div>
   );
 }
