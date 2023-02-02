22d0977113aff5836e1c856b0d57e2e509909d6a
luc0liv <luoliveiradacunha@gmail.com>
2023-02-02 20:02:12 -0300

    REQ 37: Ajuste no hook useFetch e correções em outros componentes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,14 +1,22 @@
 import { useEffect, useState } from 'react';
 
 export default function useFetch(url) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [loading, setIsLoading] = useState(null);
 
   useEffect(() => {
+    setIsLoading('Loading...');
     fetch(url)
       .then((req) => req.json())
-      .then((res) => setData(res))
-      .catch((error) => console.error(error));
+      .then((res) => {
+        setIsLoading(false);
+        setData(res);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.error(error);
+      });
   }, [url]);
 
-  return { data };
+  return { data, loading };
 }
