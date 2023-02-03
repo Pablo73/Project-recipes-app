8c6c7a972b6e58fce6850caed785968cbe6b5e3a
luc0liv <luoliveiradacunha@gmail.com>
2023-02-03 17:19:03 -0300

    Outra correção

--- src/helpers/combineIngredientsAndMeasures.js
+++ src/helpers/combineIngredientsAndMeasures.js
@@ -2,24 +2,36 @@ const combineIngredientsAndMeasures = (instructions) => {
   // Extraindo os ingredientes nas chaves que incluem strIngredient;
   const ingredients = Object.keys(instructions[0])
     .filter((key) => key.includes('strIngredient'))
-    .reduce((obj, key) => Object.assign(obj, {
-      [key]: instructions[0][key],
-    }), {});
+    .reduce(
+      (obj, key) => Object.assign(obj, {
+        [key]: instructions[0][key],
+      }),
+      {},
+    );
 
   // Extraindo as medidas nas chaves que incluem strMeasure;
   const measures = Object.keys(instructions[0])
     .filter((key) => key.includes('strMeasure'))
-    .reduce((obj, key) => Object.assign(obj, {
-      [key]: instructions[0][key],
-    }), {});
+    .reduce(
+      (obj, key) => Object.assign(obj, {
+        [key]: instructions[0][key],
+      }),
+      {},
+    );
 
   const measuresArray = Object.values(measures);
 
   const combinedValues = Object.values(ingredients)
-    .map((ingredient, index) => `${ingredient} ${!measuresArray[index]
-      ? ''
-      : measuresArray[index]}`)
-    .filter((combination) => combination !== '  ' && !combination.includes(null));
+    .map(
+      (ingredient, index) => `${ingredient} ${!measuresArray[index]
+        ? ''
+        : measuresArray[index]}`,
+    )
+    .filter(
+      (combination) => combination !== '  '
+        && combination !== ' '
+        && !combination.includes(null),
+    );
   return combinedValues;
 };
 
